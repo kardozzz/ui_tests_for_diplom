d95ce445154c97c1a77118368c95c10de53e78bf
alabzin <kard33@mail.ru>
2024-09-06 13:02:44 +0300

    create test FillForm - done!

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,15 +1,8 @@
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.commands.ScrollIntoView;
-import com.codeborne.selenide.commands.ScrollTo;
+import com.codeborne.selenide.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class TextBoxTests {
@@ -19,27 +12,56 @@ public class TextBoxTests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
 
     }
+
     @Test
-    void fillFormTests(){
+    void fillFormTests() {
+
+        String firstName = "John";
+        String lastName = "Krutoy";
+        String userEmail = "krutoy@gmail.com";
+        String userNumber = "8911222333";
+        String address = "123 Main St, Springfield";
+        String state = "NCR";
+        String city = "Delhi";
+
         open("/automation-practice-form");
-        $("#firstName").setValue("John");
-        $("#lastName").setValue("Krutoy");
-        $("#userEmail").setValue("krutoy@gmail.com");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
         $((".custom-radio:nth-child(1) > .custom-control-label")).click();
-        $("#userNumber").setValue("8911222333");
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__day--001:nth-child(1)").click();
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").click();
-        $(".option[value='1987']").scrollTo();
+        $("#userNumber").setValue(userNumber);
+        $("#dateOfBirthInput").click();  // Открыть календарь
+        $(".react-datepicker__year-select").$("option[value='1991']").click();
+        $(".react-datepicker__month-select").$("option[value='6']").click(); // Июль - это 6
+        $(".react-datepicker__day--003:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#currentAddress").setValue(address);
+        $(".custom-checkbox:nth-child(1) > .custom-control-label").click();
+        $("#uploadPicture").sendKeys("C:\\Users\\noob-master69\\Desktop\\avatar.jpg");
+        $("#state").click();
+        $("#stateCity-wrapper").$(Selectors.byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(Selectors.byText(city)).click();
+        $("#submit").click();
 
+        $(".modal-content").shouldHave(Condition.text(firstName));
 
-
-
-
+        $(".modal-content").shouldHave(Condition.text(lastName));
+        $(".modal-content").shouldHave(Condition.text(userEmail));
+        $(".modal-content").shouldHave(Condition.text(userNumber));
+        $(".modal-content").shouldHave(Condition.text("July"));
+        $(".modal-content").shouldHave(Condition.text("1991"));
+        $(".modal-content").shouldHave(Condition.text("3"));
+        $(".modal-content").shouldHave(Condition.text(address));
+        $(".modal-content").shouldHave(Condition.text(state));
+        $(".modal-content").shouldHave(Condition.text(city));
+        $(".modal-content").shouldHave(Condition.text("Sports"));
+        $(".modal-content").shouldHave(Condition.text("Maths"));
+        $("#closeLargeModal").click();
+        System.out.println("Форма успешно заполнена, значения соответствуют введеным, окно закрыто.");
     }
+
 
 }
