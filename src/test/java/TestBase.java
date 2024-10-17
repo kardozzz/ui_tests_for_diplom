0cf45dda083d452ce2d84f3f458f31fd231b21a5
alabzin <kard33@mail.ru>
2024-10-17 10:08:29 +0300

    add Attach.screenshotAs();             Attach.pageSource();             Attach.browserConsoleLogs();             Attach.addVideo();

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,5 +1,8 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -10,9 +13,16 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
     }
     @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
     void closeDriver() {
         WebDriverRunner.closeWindow();
     }
