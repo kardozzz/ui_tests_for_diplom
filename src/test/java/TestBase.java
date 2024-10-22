1601e80e511f33f9c02110bf3b7b208a862f894f
alabzin <kard33@mail.ru>
2024-10-22 10:47:32 +0300

    fix firefox parameter

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -34,8 +34,10 @@ public class TestBase {
     @AfterEach
     void addAttachments() {
         Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
+        if (!Configuration.browser.equals("firefox")) {
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+        }
         Attach.addVideo();
         closeWebDriver();
     }
