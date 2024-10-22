c8f5dc3ad9f3480dcecb31dbcc772d9bffcc7890
alabzin <kard33@mail.ru>
2024-10-22 10:12:26 +0300

    add parameters witch jenkins

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -15,7 +15,7 @@ public class TestBase {
     @BeforeAll
     static void setUp() {
         Configuration.browserSize = System.getProperty("browserSize");
-        Configuration.baseUrl = System.getProperty("baseUrl");
+        Configuration.baseUrl = System.getProperty("browserBaseUrl");
         Configuration.browser = System.getProperty("browserName");
         Configuration.browserVersion = System.getProperty("browserVersion");
         Configuration.pageLoadStrategy = "eager";
