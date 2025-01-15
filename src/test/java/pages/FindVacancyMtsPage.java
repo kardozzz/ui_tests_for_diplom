2743a39f42bcc4332c2494a67cebd75b0eb77c73
alabzin <kard33@mail.ru>
2025-01-15 12:42:33 +0300

    init commit

--- src/test/java/pages/FindVacancyMtsPage.java
+++ src/test/java/pages/FindVacancyMtsPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -10,12 +11,12 @@ public class FindVacancyMtsPage {
     final SelenideElement
             inputVacancyName = $(".h-input__inner"),
             selectVacancy = $$(".vacancy-card__label-item-inner").findBy(Condition.text("Тестирование"));
-
+    @Step("Вводим направление вакансии")
     public  FindVacancyMtsPage inputVacancy() {
         inputVacancyName.setValue("Тестирование");
         return this;
     }
-
+    @Step("Выбираем первую найденную вакансию по направлению")
     public FindVacancyMtsPage clickFindVacancy() {
         selectVacancy.shouldHave(Condition.text("Тестирование")).click();
         return this;
